372820823ef499d3ae5334cf816945777b47bf7a
sproshchaev <sproshchaev@gmail.com>
2026-07-27 21:22:18 +0500

    add test

--- m5-level-02/src/main/java/com/javarush/LegacyApp.java
+++ m5-level-02/src/main/java/com/javarush/LegacyApp.java
@@ -8,6 +8,21 @@ public class LegacyApp {
 
     public static void main(String[] args) {
 
+        // Веб приложение C(reate) R(ead) U(pdate) D(elete)
+        // http-запросы                 POST     GET    POST    DELETE
+        // Модель User
+        // -------------- Слой контроллеров (UI)
+        // Контроллер -> Сервис -> Репозиторий -> Бд (user)
+        // -------------- Сервисный слой ---------------------------------
+        // Сервис класс UserService: метод создания, чтения (ед, всех), изменение, удаление
+        // --------------- Слой репозиториев -----------------------------
+        // Репозиторий класс UserRepository: метод создания, чтения (ед, всех), изменение, удаление
+        // --------------- Уровень БД ---------------------
+        // БД PG/MySQL/Oracle/... application.properties
+
+        // Модель Book
+        // ...
+
         // Традиционный подход
         //UserService userService = new UserService();
         //userService.registerUser("Alice");
